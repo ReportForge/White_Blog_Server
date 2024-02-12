02e4eb92905839607134bbf70ad44bbc1718ec06
Danel <danel_yehuda@epam.com>
2024-02-12 17:42:45 +0200

    fix auth bag 12.2.2024

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,39 +2,36 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-
-const authController = {
-  // User registration
-  async register(req, res) {
-    try {
-      const { firstName, lastName, email, password } = req.body;
-      const hashedPassword = await bcrypt.hash(password, 12);
-      const newUser = new User({ firstName, lastName, email, password: hashedPassword });
-      await newUser.save();
-      res.status(201).json({ message: 'User created successfully!' });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
-  },
-
-  // User login
-  async login(req, res) {
-    try {
-      const { email, password } = req.body;
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(404).json({ message: 'User not found!' });
-      }
-      const isMatch = bcrypt.compare(password, user.password);
-      if (!isMatch) {
-        return res.status(400).json({ message: 'Invalid credentials!' });
-      }
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-      res.json({ token });
-    } catch (error) {
-      res.status(500).json({ message: error.message });
-    }
+// User registration
+const register = async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const newUser = await User.create({ firstName: firstName, lastName: lastName, email, password: hashedPassword });
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.status(201).json({ newUser, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-};
+}
 
-module.exports = authController;
+// User login
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found!' });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid credentials!' });
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ user, token });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+module.exports = { register, login };
--- server/models/User.js
+++ server/models/User.js
@@ -4,9 +4,9 @@ const bcrypt = require('bcryptjs');
 const UserSchema = new mongoose.Schema({
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  email: { 
-    type: String, 
-    required: true, 
+  email: {
+    type: String,
+    required: true,
     unique: true,
     lowercase: true, // Always convert email to lowercase
     trim: true // Remove whitespace from both ends
@@ -15,17 +15,6 @@ const UserSchema = new mongoose.Schema({
   isEditor: { type: Boolean, default: false } // Added field to indicate if the user is an editor
 });
 
-// Middleware to hash password before saving a user
-UserSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 8);
-  next();
-});
-
-// Method to compare provided password with hashed password
-UserSchema.methods.comparePassword = async function(candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
 
 const User = mongoose.model('User', UserSchema);
 
