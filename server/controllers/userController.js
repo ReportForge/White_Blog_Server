fdd4ab219c2d75597d9855858e0ca119b73a85d3
Danel <danel_yehuda@epam.com>
2024-02-13 12:12:48 +0200

    UserManagement 13.2.2024

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -31,6 +31,32 @@ const userController = {
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
+  },
+
+   // Set a user as an editor
+   async setUserAsEditor(req, res) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, { $set: { isEditor: true } }, { new: true });
+      if (!user) {
+        return res.status(404).json({ message: 'User not found!' });
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  },
+
+  // Remove editor status from a user
+  async removeEditorStatus(req, res) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, { $set: { isEditor: false } }, { new: true });
+      if (!user) {
+        return res.status(404).json({ message: 'User not found!' });
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   }
 
 };//end
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -9,6 +9,12 @@ router.get('/:id', isAuthenticated, userController.getUserProfile);
 // Update user profile - Only accessible by the user themselves
 router.put('/:id', isAuthenticated, userController.updateUserProfile);
 
-router.get(isAuthenticated, userController.getAllUsers);
+router.get('/',isAuthenticated, userController.getAllUsers);
+
+// Route to set a user as an editor
+router.put('/:id/set-editor', isAuthenticated, userController.setUserAsEditor);
+
+// Route to remove editor status from a user
+router.put('/:id/remove-editor', isAuthenticated, userController.removeEditorStatus);
 
 module.exports = router;
