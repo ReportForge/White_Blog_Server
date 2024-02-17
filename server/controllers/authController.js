8193a4703105daaa1123d16fb44f69d1ec8d48eb
Danel <danel_yehuda@epam.com>
2024-02-17 21:17:17 +0200

    jwt expiresIn update

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -8,7 +8,7 @@ const register = async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 12);
     const newUser = await User.create({ firstName: firstName, lastName: lastName, email, password: hashedPassword });
-    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '2h' });
     res.status(201).json({ newUser, token });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -27,7 +27,7 @@ const login = async (req, res) => {
     if (!isMatch) {
       return res.status(400).json({ message: 'Invalid credentials!' });
     }
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '2h' });
     res.json({ user, token });
   } catch (error) {
     res.status(500).json({ message: error.message });
