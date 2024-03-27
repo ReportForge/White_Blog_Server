c87d7f11bcb04ac36f5c26db8c321b322d283a1f
Danel <danel_yehuda@epam.com>
2024-03-27 11:21:24 +0200

    first heroku

--- server/server.js
+++ server/server.js
@@ -31,6 +31,6 @@ app.use('/api/users', usersRoute);
 app.use('/api/auth', authRoutes);
 
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
