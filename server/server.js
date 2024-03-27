516531b3116ec36962135178ed47dad770db7977
Danel <danel_yehuda@epam.com>
2024-03-27 11:43:43 +0200

    Port change

--- server/server.js
+++ server/server.js
@@ -31,6 +31,6 @@ app.use('/api/users', usersRoute);
 app.use('/api/auth', authRoutes);
 
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${PORT}`);
 });
