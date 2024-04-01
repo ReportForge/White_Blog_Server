ca9fd8be56b2a4aa36c060311cbea748fa24fdc6
Danel <danel_yehuda@epam.com>
2024-04-01 12:07:36 +0300

    twitter test callbackURL

--- server/passport-setup.js
+++ server/passport-setup.js
@@ -4,7 +4,7 @@ const TwitterStrategy = require('passport-twitter').Strategy;
 passport.use(new TwitterStrategy({
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: "http://localhost:3000/api/auth/twitter/callback"
+    callbackURL: "https://monumental-florentine-67199f.netlify.app/"
   },
   async (token, tokenSecret, profile, cb) => {
     // Here, you will find or create a user in your database
