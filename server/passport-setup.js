d02ad8215bb5f9affa7bb176209cf2cc126eb18d
Danel <danel_yehuda@epam.com>
2024-04-01 17:22:41 +0300

    passport-setup update

--- server/passport-setup.js
+++ server/passport-setup.js
@@ -8,18 +8,21 @@ passport.use(new TwitterStrategy({
     callbackURL: "https://whiteblog-ffb7cfa6fd24.herokuapp.com/api/auth/twitter/callback"
   },
   async (token, tokenSecret, profile, cb) => {
-    // Here, you will find or create a user in your database
-    // Check if user already exists in your database
-    console.log("Its get here");
+    console.log("Twitter auth callback function called.");
     let user = await User.findOne({ twitterId: profile.id });
     if (user) {
       return cb(null, user);
     } else {
-      // If not, create a new user in your DB
+      // Create a new user with a default password and empty names if Twitter doesn't provide them
       user = new User({
         twitterId: profile.id,
         username: profile.username,
         displayName: profile.displayName,
+        firstName: 'TwitterUser', // Default value, consider prompting the user to update it later
+        lastName: '', // Default value, consider prompting the user to update it later
+        email: `${profile.username}@twitter.com`, // Default value, consider prompting the user for a real email later
+        password: 'twitter', // Consider using a more secure default password or a password hash
+        emailVerified: true // Assuming Twitter accounts are verified
         // Add other fields as necessary
       });
 
