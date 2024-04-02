584bac418d71c80fa5d951fd05e1324a940736b9
Danel <danel_yehuda@epam.com>
2024-04-02 11:18:16 +0300

    console.log(email);

--- server/passport-setup.js
+++ server/passport-setup.js
@@ -12,6 +12,7 @@ passport.use(new TwitterStrategy({
     console.log("Twitter auth callback function called.");
     console.log(profile.emails);
     const email = profile.emails && profile.emails[0].value;
+    console.log(email);
     const nameParts = profile.displayName.split(' ');
     const firstName = nameParts[0] || 'TwitterUser'; // Use the first part as the first name, default if empty
     const lastName = nameParts.slice(1).join(' ') || ''; // Join the rest as the last name, empty if none
